a2ee0f446b4f595c5a57a4269fd2eae4fc4c50cc
Xel-na <xelvisual19@gmail.com>
2026-09-14 17:08:55 +0800

    empty

--- Bonotan.js
+++ Bonotan.js
@@ -1,24 +1,2 @@
-// How to execute "The try statement"
-try{
-    //Code that may cause an error
-}
-catch(error){
-    // Code to handle the error
-}
-//Optional ni sya
-finally{
-    //Code that always run, no matter what
-}
-
-throw "Too big";
-throw 500;
-
-function wait(){
-    let i = 2e9;
-    
-}
-
-
-
 
 
